35875f3659e4b2a6edd2089558c5ddde14a72603
SLandgra <slandgra@ucsd.edu>
2018-04-24 20:01:06 -0700

    added random word functionality along with some UX elements

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 A simple hangman game created to demonstrate to students how to use constructors.
 
+
 This is a CLI program and requires the user to npm install inquirer.
+
+List of random words was taken from DJGilmore4U's word-guess-cli
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var inquirer = require('inquirer');
 var Word = require('./word.js');
-var guessWordList = require('./list.js');
+var randomWords = require('./list.js');
 
 var question = [{
   type: 'input',
@@ -10,12 +10,13 @@ var question = [{
     if((input.length === 1) && !(Number(input))){
       return true
     }else{
+      console.log('\n')
       return false
     }
   }
 }]
 var guessedLetters = [];
-var word = new Word('book');
+var word = randomWord()
 var guesses = 7;
 function guessLetter(){
   inquirer.prompt(question).then(function(response){
@@ -24,7 +25,7 @@ function guessLetter(){
       console.log('You already guessed ' + response.guessedLetter + '!')
       console.log('---------------------------------------------------')
       console.log(word.wordDisplay().join(' '))
-      console.log('You have: ' + guesses + ' guesses remaining.')
+      console.log('You have: ' + guesses + ' guesses remaining. \n')
       guessLetter()
     }else{
       guessedLetters.push(response.guessedLetter)
@@ -32,42 +33,53 @@ function guessLetter(){
       var found = word.guess(guess);
       var output = word.wordDisplay();
       console.log(output.join(' '))
+      if(!found){
+        guesses--
+      }
       if((guesses === 0)&& (output.includes('_'))){
         console.log('-----------------------------------')
         console.log('YOU LOSE!')
         console.log('The answer was: ' + word.stringWord)
         console.log('-----------------------------------')
+        playAgain()
       }else if(output.includes('_')){
-        if(!found){
-          guesses--
-        }
-        console.log('You have: ' + guesses + ' guesses remaining.')
+        console.log('You have: ' + guesses + ' guesses remaining. \n')
         guessLetter()
       }else{
         console.log('-------')
         console.log('YOU WIN')
         console.log('-------')
-        inquirer.prompt([{
-          type: 'confirm',
-          name: 'gameStatus',
-          message: 'Play again?'
-        }]).then(function(response){
-          if(response.gameStatus === true){
-            word = new Word('random')
-            guesses = 7;
-            guessedLetters = [];
-            console.log(word.wordDisplay().join(' '))
-            console.log('You have: ' + guesses + ' guesses remaining.')
-            guessLetter()
-          }else{
-            console.log('-------------------')
-            console.log('Thanks for playing!')
-            console.log('-------------------')
-          }
-        })
+        playAgain()
       }
     }})}
-    console.log("Welcome to CLI-Hangman!")
-    console.log(word.wordDisplay().join(' '))
-    console.log('You have: ' + guesses + ' guesses remaining.')
-    guessLetter()
+
+console.log("\nWelcome to CLI-Hangman!")
+console.log(word.wordDisplay().join(' '))
+console.log('You have: ' + guesses + ' guesses remaining. \n')
+guessLetter()
+
+
+function randomWord(){
+  var indexOfWord = Math.floor(Math.random() * randomWords.length);
+  return new Word(randomWords[indexOfWord])
+}
+function playAgain(){
+  inquirer.prompt([{
+    type: 'confirm',
+    name: 'gameStatus',
+    message: 'Play again?'
+  }]).then(function(response){
+    if(response.gameStatus === true){
+      word = randomWord();
+      guesses = 7;
+      guessedLetters = [];
+      console.log(word.wordDisplay().join(' '))
+      console.log('You have: ' + guesses + ' guesses remaining. \n')
+      guessLetter()
+    }else{
+      console.log('-------------------')
+      console.log('Thanks for playing!')
+      console.log('-------------------')
+    }
+  })
+}
